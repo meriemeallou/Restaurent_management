10ca15c3dc2d09dbe35a1d314c4a309675a7aa20
meriemeallou <156241380+meriemeallou@users.noreply.github.com>
2024-03-04 21:41:00 +0100

    upadate

--- src/components/AddR.vue
+++ src/components/AddR.vue
@@ -1,6 +1,6 @@
 <template>
     <HeaderFile />
-    <form>
+    <form @submit.prevent="addR">
   <div class="mb-3">
     <label for="Radress" class="form-label">Restaurent Name</label>
     <input type="text" class="form-control" id="Rname" aria-describedby="Rname" v-model="name">
@@ -9,33 +9,38 @@
     <label for="Radress" class="form-label" >Restaurent Adress</label>
     <input type="text" class="form-control" id="Radress" aria-describedby="Radress" v-model="adress">
   </div>
-  <button type="submit" class="btn btn-primary" v-on:click="addR">Add Restaurent</button>
+  <button type="submit" class="btn btn-primary">Add Restaurent</button>
 </form>
 </template>
 <script>
-import axios from 'axios'
-import HeaderFile from './HeaderFile.vue'
-
+import axios from 'axios';
+import HeaderFile from './HeaderFile.vue';
 
 export default {
     name:'AddR',
     components:{
         HeaderFile,
     },
+    data(){
+        return {
+            name: '',
+            adress: ''
+        }
+    },
     methods:{
         async addR(){
-                if (!this.name || !this.adress ) {
-                alert('Please fill in all champs.');
-            return;
-        }
-                let result = await axios.post("http://localhost:3000/restaurents",{
-                    name:this.name,
-                    adress:this.adress,
+            try {
+                const result = await axios.post("http://localhost:3000/restaurents", {
+                    name: this.name,
+                    adress: this.adress,
                 });
-                if(result.status==201){
-                    localStorage.setItem("resto-info", JSON.stringify(result.data));
+                if(result.status === 201){
+                    this.$router.push('/');
                 }
+            } catch (error) {
+                console.error(error);
             }
+        }
     }
 }
 </script>
--- src/components/UpdateR.vue
+++ src/components/UpdateR.vue
@@ -1,47 +1,49 @@
 <template>
-    <h1>welcome to update page </h1>
-    <form>
+<h1>update page</h1>
+<form @submit.prevent="updateR">
   <div class="mb-3">
     <label for="Radress" class="form-label">Restaurent Name</label>
-    <input type="text" class="form-control" id="Rname" aria-describedby="Rname" v-model="restaurents.name" >
+    <input type="text" class="form-control" id="Rname" aria-describedby="Rname" v-model="restaurents.name">
   </div>
   <div class="mb-3">
     <label for="Radress" class="form-label" >Restaurent Adress</label>
-    <input type="text" class="form-control" id="Radress" aria-describedby="Radress" v-model="restaurents.adress" >
+    <input type="text" class="form-control" id="Radress" aria-describedby="Radress" v-model="restaurents.adress">
   </div>
-  <button type="submit" class="btn btn-primary" v-on:click="updateR">Update Restaurent</button>
+  <button type="submit" class="btn btn-primary" >Update Restaurent</button>
 </form>
 </template>
 <script>
-import axios from 'axios'
+import axios from 'axios';
 export default {
-    name:'UpdateR',
-    data(){
-        return{
-            name:'',
-            restaurents:
-                {
+ name:'updateR',
+ data(){
+        return {
+            restaurents :{
                 name:'',
-                adress:'',
-            },
+                address:''
+            }
         }
     },
     methods:{
         async updateR(){
-                let result = await axios.put("http://localhost:3000/restaurents/"+this.$route.params.id,{
-                    name:this.restaurents.name,
-                    adress:this.restaurents.adress
-                });
-                if(result.status==200){
-                    this.$router.push('/')
+            try {
+                const response = await axios.put('http://localhost:3000/restaurents/' + this.$route.params.id, this.restaurents);
+                if (response.status === 200) {
+                    // Redirection vers la page d'accueil après la mise à jour réussie
+                    this.$router.push('/');
                 }
+            } catch (error) {
+                console.error(error);
+            }
         }
     },
     async mounted(){
-        let result = await axios.get('http://localhost:3000/restaurents/'+this.$route.params.id)
-        console.warn(result)
-        this.restaurents=result.data
-
+      try {
+        const result = await axios.get('http://localhost:3000/restaurents/' + this.$route.params.id);
+        this.restaurents = result.data;
+      } catch (error) {
+        console.error(error);
+      }
     }
 }
 </script>
